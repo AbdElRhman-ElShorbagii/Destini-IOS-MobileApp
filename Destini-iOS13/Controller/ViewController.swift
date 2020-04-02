a9776de316369d07cd7dedad6e8451af196b7a02
AbdElRhman-ElShorbagii <abdelrhmanelshorbagii@gmail.com>
2020-04-02 18:15:50 +0200

    release v.1.0.0

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUi()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        storyBrain.nextStory(sender.currentTitle!)
+        
+        updateUi()
+    }
+    
+    func updateUi(){
+                
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
 }
 
